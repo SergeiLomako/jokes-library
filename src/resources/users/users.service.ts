6683ff6fb876384cfcb3beaed7236fbe70c915b9
SergeiLomako <flameseeker23@gmail.com>
2019-12-17 16:52:46 +0200

    feat(): added hashing passwords for users

--- src/resources/users/users.service.ts
+++ src/resources/users/users.service.ts
@@ -1,18 +1,27 @@
 import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { HashService } from '../../services/hash/hash.service';
 import { User } from './interfaces/user.interface';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+    constructor(
+        @InjectModel('User') private readonly userModel: Model<User>,
+        private readonly hashService: HashService,
+    ) {}
 
     async create(user: User): Promise<User> {
+        user.password = await this.hashService.make(user.password);
         const createdUser = new this.userModel(user);
         return await createdUser.save();
     }
 
     async update(id: string, user: User): Promise<User> {
+        if (user.password) {
+            user.password = await this.hashService.make(user.password);
+        }
+
         return await this.userModel.findByIdAndUpdate(id, user, { new: true });
     }
 
