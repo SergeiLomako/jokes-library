f1ce25054e45a859520f2df8fb2964955426fa62
SergeiLomako <flameseeker23@gmail.com>
2019-12-17 13:47:40 +0200

    feat(): added ConfigService

---
+++ src/config/config.service.ts
@@ -0,0 +1,53 @@
+import * as dotenv from 'dotenv';
+import * as Joi from '@hapi/joi';
+import * as fs from 'fs';
+
+export interface EnvConfig {
+    [key: string]: string;
+}
+
+export class ConfigService {
+    private readonly envConfig: EnvConfig;
+
+    constructor() {
+        const config = fs.existsSync('.env') ? dotenv.parse(fs.readFileSync('.env')) : this.parseEnv();
+        this.envConfig = this.validateInput(config);
+    }
+
+    private parseEnv(): EnvConfig {
+        const ENV_VARIABLES = [
+            'NODE_ENV',
+            'HOST',
+            'PORT',
+            'API_KEY',
+            'MONGO_URL',
+            'JWT_EXPIRES',
+        ];
+        return ENV_VARIABLES.reduce((obj, variable) => {
+            obj[variable] = process.env[variable];
+            return obj;
+        }, {});
+    }
+
+    private validateInput(envConfig: EnvConfig): EnvConfig {
+        const envVarsSchema: Joi.ObjectSchema = Joi.object({
+            NODE_ENV: Joi.string()
+                .valid('development', 'production', 'testing')
+                .default('development'),
+            HOST: Joi.string().default('localhost'),
+            PORT: Joi.number().default(3000),
+            API_KEY: Joi.string().min(30).max(50).required(),
+            MONGO_URL: Joi.string().default('mongodb://localhost/nest'),
+            JWT_EXPIRES: Joi.string().default('1h'),
+        });
+        const { error, value: validatedEnvConfig } = envVarsSchema.validate(envConfig);
+        if (error) {
+            throw new Error(`Config validation error: ${error.message}`);
+        }
+        return validatedEnvConfig;
+    }
+
+    get(key: string): string {
+        return this.envConfig[key];
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(bodyParser.json({limit: '1mb'}));
+  app.use(bodyParser.urlencoded({
+    limit: '1mb',
+    extended: true,
+  }));
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
