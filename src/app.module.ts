accce3d214c87654cd18e8e9cef1c8e731e26336
SergeiLomako <flameseeker2304@gmail.com>
2019-12-21 13:53:37 +0200

    fix(AppModule): removed CommandModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,6 @@ import { AuthModule } from './services/auth/auth.module';
 import { UsersModule } from './resources/users/users.module';
 import { JokesModule } from './resources/jokes/jokes.module';
 import { CommentsModule } from './resources/comments/comments.module';
-import { CommandModule } from 'nestjs-command';
 import { JokeSeed } from './database/seeds/jokes.seed';
 
 @Module({
@@ -20,7 +19,6 @@ import { JokeSeed } from './database/seeds/jokes.seed';
         UsersModule,
         JokesModule,
         CommentsModule,
-        CommandModule,
     ],
     controllers: [AppController],
     providers: [AppService, JokeSeed],
